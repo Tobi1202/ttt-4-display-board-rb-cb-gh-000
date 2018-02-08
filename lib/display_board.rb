fdec12c3f903c7a18886c0aa3243b1abab41f795
TobiasBrodersen Brodersen <thetobiasbrodersen@gmail.com>
2018-02-08 12:17:55 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
-  row1 = " #{board[0]} |   |   "
+  row1 = " #{board[0]} | #{board[1]} | #{board[2]}"
   sepeartor = "-----------"
   puts "#{row}\n#{sepeartor}\n#{row}\n#{sepeartor}\n#{row}"
 end
